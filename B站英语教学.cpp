28437940fa15fefa3f25ce3bf4f5baed102a68db
Javen9527 <75876605+Javen9527@users.noreply.github.com>
2021-02-15 13:55:36 +0800

    Update B站英语教学.cpp

--- B站英语教学.cpp
+++ B站英语教学.cpp
@@ -46,12 +46,129 @@
     所以解决方案是：将取值top和弹出pop操作结合在一个函数里面。
     
 5. 死锁
+  
+  ① 产生：例如使用两把锁时
+  class ResourceMu
+  {
+    std::mutex _mu1;
+    std::mutex _mu2;
+    ofstream _f;
+  public:
+    ResourceMu(){_f.open("file.tex");}
+    void shared_print_1(string s, int v)
+    {
+      std::lock_guard<std::mutex> locker_1(_mu1);
+      std::lock_guard<std::mutex> locker_2(_mu2);
+      _f << "From " << s << ":" << v << endl; 
+    }
+    void shared_print_2(string s, int v)
+    {
+      std::lock_guard<std::mutex> locker_2(_mu2);
+      std::lock_guard<std::mutex> locker_1(_mu1);
+      _f << "From " << s << ":" << v << endl; 
+    }
+  };
+  
+  void function_thread(ResourceMu& rm) // 子线程调用
+  {
+    for(int i = 0; i < 100; ++i)
+    {
+      rm.shared_print_1("thread", i);
+    }
+  }
+
+  void function_main(ResourceMu& rm) // 主线程调用
+  {
+    for(int i = 0; i > -100; --i)
+    {
+      rm.shared_print_2("mian", i);
+    }
+  }
+
+  int main()
+  {
+    ResourceMu rm;
+    std::thread t1(function_thread, std::ref(rm));
+    function_main();
+    
+    t1.join();
+    return 0;
+  }
+
+  ② 避免这种情况的死锁：保证每次上锁的顺序一致。C++提供了保证上锁顺序的机制：std::lock(_mu1, _mu2);
+  std::lock(_mu1, _mu2);
+  std::lock_guard<std::mutex> locker_1(_mu1, std::adopt_lock);
+  std::lock_guard<std::mutex> locker_2(_mu2, std::adopt_lock);
+  
+6. unique_lock<std::mutex> 比起lock_guard更有灵活性： 
+  ① 可以直接解锁
+  std::unique_lock<std::mutex> locker(_mu);
+  //...需要上锁的代码
+  locker.unlock();
+  //...不需要上锁的代码
+  
+  ② 可以先创建锁而不上锁：
+  std::unique_lock<std::mutex> locker(_mu, std::defer_lock);
+  //...不需要上锁的代码
+  lockder.lock();
+  //...需要上锁的代码
+
+  ③ unique_lock和lock_guard都不可以被复制，但是unique_lock可以被move
+  
+  ④ unique_lock具有灵活性的代价是性能相较而言更低。
       
-      
-      
-      
-      
-      
+7. Lazy Initialization(Initialization Upon First Use Idiom)
+  
+  ① 什么是Lazy Initialization
+  class LogFile
+  {
+    std::mutex _mu;
+    std::mutex _mu_open;
+    ofstream _f;
+  public:
+    LogFile(){_f.open("Log.txt");};//每次创建类则打开一个文件，但是我们想要的是在使用shared_print函数时候才打开文件
+    void shared_print(){...}
+  }
+  将打开文件操作移动到shared_print()中，即为Lazy Initialization；
+  
+  ② 考虑线程安全：
+  void shared_print(string id, int value)
+  {
+    //打开文件：只被打开一次
+    if(!_f.isopen())
+    {
+      std::unique_lock<std::mutex> locker_open(_mu_open);//这不是线程安全的（*）
+      _f.open("Log.txt");
+    }
+    
+    //...操作_f的代码，会调用多次
+    std::unique<std::mutex> locker(_mu);
+    ...
+  }
+  分析为什么不是线程安全的：thread1发现_if未打开，所以给_mu_open上锁（还未打开文件），此时执行至thread2发现文件未打开，则等待给_mu_open上锁，一旦thread1打开文件并释放锁之后，thread2再次打开文件。
+
+  ③ 解决上述问题:
+  void shared_print(string id, int value)
+  {
+    //打开文件：只被打开一次
+    {
+      std::unique_lock<std::mutex> locker_open(_mu_open);//该代码则保证文件只打开一次，是线程安全的。
+      if(!_f.isopen())
+      {
+        _f.open("Log.txt");
+      }
+    }
+ 
+    //...操作_f的代码，会调用多次
+    std::unique<std::mutex> locker(_mu);
+    ...
+  }
+  
+  引入多次上锁解锁的操作带来的资源浪费问题：
+  
+  ④ 解决上述问题，使用once_flag
+  std::once_flag _flag;
+  std::call_once(_flag, [&](){_f.open("Log.txt");}); //thread safe and efficient.
       
       
       
