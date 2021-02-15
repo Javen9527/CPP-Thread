2383f9aa4f80e31e670960b33cc8dd223163cd6b
Javen9527 <75876605+Javen9527@users.noreply.github.com>
2021-02-15 21:06:48 +0800

    Update B站英语教学.cpp

--- B站英语教学.cpp
+++ B站英语教学.cpp
@@ -170,9 +170,246 @@
   std::once_flag _flag;
   std::call_once(_flag, [&](){_f.open("Log.txt");}); //thread safe and efficient.
       
-      
-      
-      
-      
-      
-      
+8. 条件变量    
+  ① 生产者消费者模型引入的问题
+  void function_1()//生产者
+  {
+    int count = 10;
+    while(count > 0)
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      q.push_front(count);
+      locker.unlock();
+      std::this_thread::sleep_for(chrono::seconde(1));
+      --count;
+    }
+  }
+  void function_2()//消费者
+  {
+    int data = 0;
+    while(data != 1)//该模型中消费者将一直循环判断q是否为空，导致低效
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      if(!q.empty())
+      {
+        data = q.back();
+        q.pop_back();
+        locker.unlock();
+        cout << "thread2 got a number from thread1 :" << data << endl;
+      }
+      else
+      {
+        locker.unlock();
+        std::this_thread::sleep_for(chrono::milliseconds(10));//通过等待减少循环次数
+      }
+    }
+  }
+  其中的问题是消费者一直循环判断导致的低效问题，可以通过在q为空时候等待的方式来减少循环。但是并不好判断需要等待多久。
+
+  ② 引入条件变量
+  void function_1()
+  {
+    int count = 10;
+    while(count > 0)
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      q.push_front(count);
+      locker.unlock();
+      cond.notify_one();//当生产一个数据后，给出通知
+      std::this_thread::sleep_for(chrono::seconds(1));
+      --count;
+    }
+  }
+  void function_2()
+  {
+    int data = 0;
+    while(data != 1)
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      cond.wait(locker);//等待唤醒
+      data = q.back();
+      q.pop_back();
+      locker.unlock();
+      cout << "Thread 2 got a value from Thread1: " << data << endl;
+    }
+  }
+  ·添加条件变量：std::condition_variable cond;
+  ·在function_1中，添加cond.notify_one();
+  ·在function_2中，删除对q.empty()的判断，而使用cond.wait(locker); 为什么wait需要locker参数？因为cond.wait将进入sleep状态，无需锁住任何资源，需要对locker解锁。当被唤醒时候，再次上锁。
+  ·针对条件变量，注意需要使用unique_lock而不是lock_guard，因为会对locker多次解锁和上锁。
+  ·防止function_2可能自己唤醒（spurious wake）：cond.wait(locker, [](){return !q.empty();});
+  
+9. Future and Promise      
+  ① 父线程从子线程获得数据
+  常规做法是：
+  void function(int N, int& x)
+  {
+    int res = 1;
+    for(int i = N; i > 1; --i)
+      res *= i;
+    x = res;
+  }
+  int main()
+  {
+    int x;
+    std::thread t 1(function, 4, std::ref(x));
+    
+    t1.join();
+    return 0;
+  }
+  此时，由于父线程和子线程共同使用x，则需要使用mutex，同时，需要保证由子线程产出x后父线程才获取x，所以需要条件变量。
+  
+  ② 更好的做法是：
+  使用函数std::async(); 其第一个参数是可调用对象，后面的参数是可调用对象的参数（和线程参数一致）。其返回一个std::future<T>对象，T是可调用对象的返回值类型。
+  int function(int N)
+  {
+    int res = 1;
+    for(int i = N; i > 1; --i)
+      res *= i;
+    return res;
+  }
+  int main()
+  {
+    int x;
+    std::future<int> fu = std::async(function, 4);//async是否创建一个新线程取决于参数：std::launch::deferred参数将不会使async函数创建新线程，使用std::launch::async则会创建新线程，或者使用两者的和|
+    x = fu.get();//注意，get只能被调用一次
+    
+    return 0;
+  }
+
+  ③ 通过使用promise给出一个future，将父线程的值传给子线程
+  int Myfunction(std::future<int>& f)//需要使用引用，因为future不能被复制，或者使用shared_future
+  {
+    int res = 1;
+    int N = f.get();//只能调用一次
+    for(int i = 1; i <= N; ++i)
+    {
+      res *= i;
+    }
+    return res;
+  }
+  int main()
+  {
+    int x; 
+    
+    //并不知道要给Myfunction传入什么值
+    std::promise<int> p;
+    std::future<int> f = p.get_future();
+    
+    //std::future<int> fu = std::async(Myfunction, 4);//此时并不知道是否需要传入4，则通过promise在未来决定
+    std::future<int> fu = std::async(Myfunction, std::ref(f));
+    
+    //...执行一些代码后才知道需要给Myfunction传入什么值
+    p.set_value(4);
+    
+    x = fu.get();
+    
+    return 0;
+  }
+
+  ④ future和promise 与 unique_lock一样，只能move不能复制 
+  上述引入的问题：如果多个线程调用Myfunction，则会多次调用f.get()导致编译失败。使用shared_future可以避免该问题。
+  std::shared_future<int> sf = f.share();
+
+10. Callable object总结
+  ① thread对象：std::thread t1(callableOjb, param)
+  ② async函数：std::async(std::launch::async, callableObj, param)
+  ③ bing函数：std::bind(callableObj, param)
+  ④ call_once函数：std::call_once(once_flag, callableObj, param)
+
+11. 针对可调用对象的用法总结：
+  class A
+  {
+   public:
+    void f(int x, char c){}
+    long g(double x){return 0;}
+    int operator()(int N){return 0;}
+  };
+  void foo(int x){}
+  int main()
+  {
+    A a;
+    std::thread t1(a, 6); //复制a对象，执行operator()(int N);
+    std::thread t2(std::ref(a), 6); //引用a对象，执行operator()(int N);
+    std::thread t3(std::move(a), 6); //移动a对象，执行operator()(int N)，注意a不能再在主线程中被使用；
+    
+    std::thread t4(A(), 6); //创建临时对象；
+    std::thread t5(&A::f, a, 8, 'w'); // 复制a对象，执行a.f()；
+    std::thread t6(&A::f, &a, 8, 'w'); // 引用a对象，执行a.f();
+    
+    std::thread t7([](int x){return x*x;}, 6); //使用lamda函数；
+    std::thread t8(foo, 7);
+  }
+
+12. packaged task  
+
+  ① 引入packaged task
+  int factorial(int N)
+  {
+    int res = 1;
+    for(int i = N; i > 1; --i)
+      res *= i;
+    return res;
+  }
+  int main()
+  {
+    std::packaged_task<int(int)> t(factorial);//注意不能像thread一样传入factorial的参数，需要使用bind：std::bind(factorial, 6);
+    ///...do something els
+    t(6);
+    int x = t.get_future().get();
+    
+    return 0;
+  }
+  注意，使用bind后，需要变成：packaged_task<int()>，同时调用时候只能t();
+  使用bind时候也可以这样：
+  auto t = bind(factorial, 6);
+  //...
+  t();
+  
+  ② package_task的应用场景
+  std::deque<std::packaged_task<int()>> task_q;//任务队列
+  std::mutex mu;//task_q被主线程和子线程共同使用，需要锁防止同时使用
+  std::condition_variable cond;
+  int factorial(int N)
+  {
+    int res = 1;
+    for(int i = N; i > 1; --i)
+      res *= i;
+    return res;
+  }
+ 
+  void thread_1()//在某个线程中执行任务
+  {
+    std::packaged_task<int()> t;
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      cond.wait(locker, [](){return !task_q.empty();});//当任务列表里面有任务才唤醒
+      t = std::move(task_q.front()); 
+      task_q.pop_front();
+    }
+    t();
+  }
+
+  int main()
+  {
+    std::thread t1(thread_1);
+    std::packaged_task<int()> t(bind(factorial, 6));
+    std::future<int> fu = t.get_future();//获取任务执行结果
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      task_q.push_back(std::move(t));
+    }
+    cond.notify_one();
+    
+    cout << fu.get() << endl;//查看结果
+    t1.join();
+    return 0;
+  }
+
+12. 获得future总结
+  ① promise::get_future();
+  ② package_task::get_future();
+  ③ async() return a future;
+  
+  
+
